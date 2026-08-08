8036f4eae3bb3e182dd73e5544794e787f8b8935
moha700m <64600035+moha700m@users.noreply.github.com>
2026-08-08 12:30:04 +0300

    Route admin and mount CMS bridge

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import AdminApp from './admin/AdminApp';
+import CmsRuntimeBridge from './components/CmsRuntimeBridge';
 import EnhancementLayer from './components/EnhancementLayer';
 import './index.css';
 import './interactive.css';
 import './superstack.css';
 
+const isAdminRoute = window.location.pathname === '/admin' || window.location.pathname.startsWith('/admin/');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <EnhancementLayer />
-    <App />
+    {isAdminRoute ? <AdminApp /> : <><CmsRuntimeBridge/><EnhancementLayer/><App/></>}
   </StrictMode>
 );
