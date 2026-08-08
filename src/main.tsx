1a956a9c00b21bf944e103dca038cdb8a9d2917a
moha700m <64600035+moha700m@users.noreply.github.com>
2026-08-08 10:53:16 +0300

    Mount interactive enhancement layer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import EnhancementLayer from './components/EnhancementLayer';
 import './index.css';
+import './interactive.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <EnhancementLayer />
     <App />
   </StrictMode>
 );
